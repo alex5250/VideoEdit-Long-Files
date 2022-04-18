c87efec8f23359be9ee0f23be7b06cd06f7949c5
Alex Zaslavskis <sahsariga111@gmail.com>
2022-04-19 00:47:54 +0300

    Create SplitVideo.lua

---
+++ SplitVideo.lua
@@ -0,0 +1,72 @@
+require 'mp'
+require 'mp.msg'
+
+-- Copy the current time of the video to clipboard.
+
+WINDOWS = 2
+UNIX = 3
+
+runned_times=0
+
+local function divmod(a, b)
+    return a / b, a % b
+end
+
+
+
+local function write_to_file(time,way)
+		file = io.open("config.dat", "a")
+	    file:write(string.format("%s_%s=%s \n",runned_times,way, time))
+	    file:close()
+	-- body
+end
+local function make_out_file_begin(time,way)
+	if runned_times == 0
+		then
+			mp.osd_message(string.format("new .dat file  %s", video_path))
+			local video_path = mp.get_property("path")
+			file = io.open("config.dat", "w")
+		    file:write(string.format("[ %s ] \n", video_path))
+		    file:close()
+
+		    write_to_file(time,way)
+	else
+    	
+		write_to_file(time,way)
+    end
+    runned_times=runned_times+1
+	-- body
+end
+
+local function make_out_file_end(time,way)
+    	write_to_file(time,way)
+end
+
+local function get_time()
+	local video_path = mp.get_property("path")
+    local time_pos = mp.get_property_number("time-pos")
+    local minutes, remainder = divmod(time_pos, 60)
+    local hours, minutes = divmod(minutes, 60)
+    local seconds = math.floor(remainder)
+    local milliseconds = math.floor((remainder - seconds) * 1000)
+    local time = string.format("%02d:%02d:%02d.%03d", hours, minutes, seconds, milliseconds)
+    mp.osd_message(string.format("Start sample %s", time))
+    make_out_file_begin(time,"begin")
+   
+end
+
+local function finish_time()
+    local time_pos = mp.get_property_number("time-pos")
+    local minutes, remainder = divmod(time_pos, 60)
+    local hours, minutes = divmod(minutes, 60)
+    local seconds = math.floor(remainder)
+    local milliseconds = math.floor((remainder - seconds) * 1000)
+    local time = string.format("%02d:%02d:%02d.%03d", hours, minutes, seconds, milliseconds)
+    mp.osd_message(string.format("Ended sample %s", time))
+    make_out_file_end(time,"end")
+end
+
+
+
+mp.add_key_binding("c", "get_time", get_time)
+mp.add_key_binding("e", "finish_time", finish_time)
