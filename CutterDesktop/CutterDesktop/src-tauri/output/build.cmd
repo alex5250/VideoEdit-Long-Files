2d82d97e1685285725f5655482d6583727c35534
Alex Zaslavskis <sahsariga111@gmail.com>
2022-04-26 22:51:25 +0100

    Delete build.cmd

--- CutterDesktop/CutterDesktop/src-tauri/output/build.cmd
+++
@@ -1,6 +0,0 @@
-ffmpeg -ss 00:01:16.480 -to 00:01:30.040  -i C0001.MP4 -vcodec copy WTF.mp4
-ffmpeg -ss error -to error  -i C0001.MP4 -vcodec copy error.mp4
-ffmpeg -ss 00:01:43.680 -to 00:01:49.580  -i C0001.MP4 -vcodec copy Арсений_и_матвей.mp4
-ffmpeg -ss 00:01:06.880 -to 00:01:23.600  -i C0001.MP4 -vcodec copy помогите.mp4
-ffmpeg -ss 00:02:08.040 -to 00:02:27.020  -i C0001.MP4 -vcodec copy ахахаха.mp4
-ffmpeg -ss error -to error  -i C0001.MP4 -vcodec copy error.mp4
